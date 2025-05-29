e8f56a89bc615d452359bc1dd1c283e0e20ade8f
Jorge Balarezo <jbalarezocarden@gmail.com>
2025-05-29 13:48:36 -0500

    Corrige fondo de sección y enlaces en Astro

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://alosistemas.github.io/alosistemaslanding/',
+    base: 'alosistemaslanding',
+});
